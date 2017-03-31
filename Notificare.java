654a7f9c2ac7e5887363645d845e5a78a3c70c0b
teodorfrunza <teodor.frunza@info.uaic.ro>
2017-03-31 08:11:24 +0300

    Last time
    asd

---
+++ Notificare.java
@@ -0,0 +1,43 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   Notificare.java
+
+
+public class Notificare
+{
+
+    public Notificare()
+    {
+    }
+
+    public void priorityNotif()
+    {
+    }
+
+    public void editNotif()
+    {
+    }
+
+    public void postNotif()
+    {
+    }
+
+    public void saveNotif()
+    {
+    }
+
+    public void hideNotif()
+    {
+    }
+
+    private void getSubject()
+    {
+    }
+
+    private String Subject;
+    private String mesaj;
+    private String data;
+    private Boolean priority;
+    private String expeditor;
+}
---
+++ Primarie.java
@@ -0,0 +1,23 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+// Source File Name:   Primarie.java
+
+import java.util.Vector;
+
+public class Primarie
+{
+
+    public Primarie()
+    {
+    }
+
+    private void generateNotif()
+    {
+    }
+
+    public String Nume;
+    public int IDPrimarie;
+    public Vector myNotificare;
+    public Vector myPrimarie;
+}
